12a3983c0baa618f574eaa7ef193936e245120f6
Kaushik Das <kaushik.das@codeclouds.in>
2022-07-08 18:39:53 +0530

    Modified commit for config

--- config.php
+++ config.php
@@ -14,9 +14,9 @@ $conn = mysql_connect("localhost","root","root")
  */
 
 $databaseHost = 'localhost';
-$databaseName = 'test';
-$databaseUsername = 'root';
-$databasePassword = 'root';
+$databaseName = 'crudapp';
+$databaseUsername = 'admincrud';
+$databasePassword = 'test123';
 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
  
